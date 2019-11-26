cf3ab7ab76fac430d22852aabcffed18c6cf782c
Mark Hanlon <mark@markhanlon.com>
2019-11-26 22:39:39 +0000

    Day 1 solved

---
+++ Day1/Part1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string line = "0";
+            int total = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    while (!String.IsNullOrWhiteSpace(line))
+                    {
+                        line = sr.ReadLine();
+                        int val;
+                        if (Int32.TryParse(line, out val))
+                        {
+                            total += val;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught: {ex.Message}");
+                return;
+            }
+            
+            Console.WriteLine($"Answer is {total}");
+        }
+    }
+}
---
+++ Day1/Part2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int total = 0;      
+            int fileLoops = 0;  
+            int? result = null;
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            dict.Add(total, true);
+
+            try
+            {                
+                while (result is null)
+                {
+                    fileLoops++;
+                    using (StreamReader sr = new StreamReader("input.txt"))
+                    {
+                        string line = "0";
+                        while (!String.IsNullOrWhiteSpace(line))
+                        {
+                            line = sr.ReadLine();
+                            int val;
+                            if (Int32.TryParse(line, out val))
+                            {
+                                total += val;
+                                if (dict.ContainsKey(total))
+                                {
+                                    result = total;
+                                    break;
+                                }
+                                else
+                                {
+                                    dict.Add(total, true);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception caught: {ex.Message}");
+                return;
+            }
+            
+            Console.WriteLine($"Answer is {result}, after {fileLoops} loops");            
+        }
+    }
+}
